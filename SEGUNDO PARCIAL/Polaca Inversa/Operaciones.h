a9a426bfc9774d10f1747356718f4633a93dd031
Milena Verenice Maldonado Córdova <105258677+MilenaMaldonado@users.noreply.github.com>
2023-07-01 23:20:39 -0500

    Create Operaciones.h

---
+++ SEGUNDO PARCIAL/Polaca Inversa/Operaciones.h
@@ -0,0 +1,22 @@
+/***********************************************************************
+ * Module:  Operaciones.h
+ * Author:  Ronny Ibarra, Milena Maldonado, Daniel Guaman
+ * Modified: jueves, 02 de Julio de 2023 
+ * Purpose: Polaca Inversa
+ ***********************************************************************/
+ #pragma once
+
+class Operaciones
+{
+public:
+	double sin(double);
+
+	double cos(double);
+
+	double tan(double);
+
+	long factorial(int);
+
+	double potencia(double, double);
+};
+
