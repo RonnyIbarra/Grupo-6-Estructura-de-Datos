4d5b313cd9608344eace4c70478d93a9b494d77d
Milena Verenice Maldonado Córdova <105258677+MilenaMaldonado@users.noreply.github.com>
2023-07-09 23:38:24 -0500

    Update Validacion.cpp

--- SEGUNDO PARCIAL/Polaca Inversa/Code/PolacaCalculadora/Validacion.cpp
+++ SEGUNDO PARCIAL/Polaca Inversa/Code/PolacaCalculadora/Validacion.cpp
@@ -14,27 +14,39 @@ Validacion::Validacion()
 {
 }
 
-
-char* Validacion::ingresoExpresion()
-{
-    char* a = new char[0];
+char* Validacion::ingresoExpresion(char const *msj)
+ {
+ 
+           char* a = new char[0];
     char c;
     int i = 0;
-    while ((c = _getch()) != 13)
-    {
-        if (c >= '0' && c <= '9' || c == 113 || c == 114 || c == 115 || c == 116 || c == 99
-            || c == 47 || c == 42 || c == 43 || c == 45 || c == 46
-            || c == 94)
-        {
+    bool esPunto = false;
+    
+    cout << msj;
+    while ((c = getch()) != 13) {
+          if (c >= '0' && c <= '9' || c == 113|| c == 114 || c == 115 || c == 116|| c == 99
+            || c == 47||c == 42 || c == 43|| c == 45 
+            || c == 94 ) {
             cout << c;
             a[i++] = c;
+            esPunto = false;
         }
-        else if (c == '\b')
-        {
+        else if (c == '.' && !esPunto) {
+            cout << c;
+            a[i++] = c;
+            esPunto = true;
+        }
+        else if (c == '\b') {
             i--;
+            if (a[i] == '.') {
+                esPunto = false;
+            }
             cout << "\b \b";
+        }
+        if (i == 0 && (a[0] >= 'a' && a[0] <= 'z')) {
+            a[0] = a[0] - 32;
         }
     }
     a[i] = '\0';
     return a;
-}
+    }
