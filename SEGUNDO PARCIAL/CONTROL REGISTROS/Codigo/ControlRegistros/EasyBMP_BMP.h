e338a1f5bcb973ebd130c68c30f58e120030faf6
Milena Verenice Maldonado Córdova <105258677+MilenaMaldonado@users.noreply.github.com>
2023-07-23 19:41:23 -0500

    Update EasyBMP_BMP.h

--- SEGUNDO PARCIAL/CONTROL REGISTROS/Codigo/ControlRegistros/EasyBMP_BMP.h
+++ SEGUNDO PARCIAL/CONTROL REGISTROS/Codigo/ControlRegistros/EasyBMP_BMP.h
@@ -1,21 +1,10 @@
-/*
-UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
-DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
-INGENIERIA DE SOFTWARE             
-	AUTORES:
-		Johan Romo
-		Cristhopher Villamarin
-		Sebastian Torres
-                Jeanhela Nazate
-                Milena Maldonado
-                Shared Tinoco
-		Brandon Masacela
-		Juan Reyes
-        GRUPOS: 7 - 10 - 11 - 14
-        FECHA DE CREACION:        Viernes, 1 de julio de 2022 7:34:00 p. m. 	
-	FECHA DE MODIFICACION:    Martes, 19 de junio de 2022 4:58:00 p. m.
-        PROPOSITO: Proyecto Segundo Parcial - Arboles B
-*/
+/***********************************************************************
+ * UNIVERSIDAD DE LAS FUERZAS ARMADAS - ESPE 
+ * Module:  main.cpp
+ * Author:  Milena Maldonado, Alexander Guaman, Ronny Ibarra
+ * Modified:  Lunes, Julio 17, 2023 18:24:36
+ * Purpose: Control Registos 
+ ***********************************************************************/
 
 #ifndef _EasyBMP_BMP_h_
 #define _EasyBMP_BMP_h_
